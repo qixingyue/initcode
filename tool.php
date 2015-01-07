00c58e385a5a8678ea4ac45a790d362be7657b3c
qixingyue <qixingyue@126.com>
2015-01-07 17:18:31 +0800

    添加可执行文件头

--- tool.php
+++ tool.php
@@ -6,6 +6,7 @@ include ROOT . '/smarty/Smarty.class.php';
 
 $smarty = new Smarty();
 $smarty->template_dir = ROOT . "/templates/";
+$smarty->compile_dir = ROOT . "/templates_c/";
 $smarty->assign("dir",$argv[1]);
 $smarty->assign("filename",$argv[2]);
 $smarty->display("tool.tpl");
